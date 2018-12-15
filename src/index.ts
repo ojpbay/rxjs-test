4030f42b2ef3549074d5b3631a4cfac69a35a731
Oliver Baylis <oli.baylis@gmail.com>
2018-12-15 19:11:28 +0000

    Add cursor move fromevent

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Observable } from 'rxjs';
+import { fromEvent } from 'rxjs';
+
 var observable = Observable.create((observer: any) => {
   observer.next('Hello World!');
   observer.next('Hello Again!');
@@ -12,9 +14,15 @@ observable.subscribe(
   () => logItem('Completed')
 );
 
+var mouseObservable$ = fromEvent(document.body, 'mousemove').subscribe(
+  (e: any) => {
+    logItem(`x:${e.pageX}, y:${e.pageY}`);
+  }
+);
+
 function logItem(val: any) {
   var node = document.createElement('li');
   var textnode = document.createTextNode(val);
   node.appendChild(textnode);
-  document.getElementById('list').appendChild(node);
+  document.getElementById('list').prepend(node);
 }
